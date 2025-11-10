f21714e80922796a93f4d990b126d18657eb085a
Mokshasree <moksha@unc.edu>
2025-11-10 00:14:58 -0500

    All recent changes

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,9 +1,2 @@
-# FinSight/src/app/__init__.py
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-from .api.v1 import routes
-
-app.include_router(routes.router)
+""" FinSight app package init. """
+__all__ = []
